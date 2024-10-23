2f7f8c22f9bd7299a5fe595861a84390b942f7a4
Coding-Chobo <dbdaldrb1@gmail.com>
2024-10-23 22:35:40 +0900

    리소스 다듬기 및 출력

--- object_1.py
+++ object_1.py
@@ -3,32 +3,38 @@ import random
 # Game object class here
 class Map:
     def __init__(self):
-        self.image = load_image('basic_map.png')
-        self.menu_sprite = load_image('menu_sprite.png')
+        self.image = load_image('resource/basic_map.png')
+        self.menu_sprite = load_image('resource/menu_sprite.png')
         self.world_width = 800
         self.world_height = 800
         self.menu_size = 64
         self.menu_gap = 92
-        self.frame = [0,0,0,0,0,0]
-        self.data = [[1,1,1,1,1,1,0,4],
-                     [1,2,0,2,0,2,0,3],
-                     [1,1,0,1,0,1,0,3],
-                     [0,0,0,0,0,0,0,3],
-                     [0,0,0,0,0,0,0,3],
-                     [1,1,0,1,0,1,0,3],
+        self.menu_frame = 0
+        self.data = [[1,1,1,1,1,1,0,9],
+                     [1,2,0,2,0,2,0,8],
+                     [1,1,0,1,0,1,0,7],
+                     [0,0,0,0,0,0,0,6],
+                     [0,0,0,0,0,0,0,5],
+                     [1,1,0,1,0,1,0,4],
                      [1,2,0,2,0,2,0,3],
                      [1,1,1,1,1,1,1,1]
                      ]
+        self.get_table_limit_time = 600
+        self.get_table_time = 0
 
     def draw(self):
         self.image.clip_draw(0,0,self.world_width,self.world_height,self.world_width / 2,self.world_height / 2)
         for i in range(0,6):
-            self.menu_sprite.clip_draw(i * self.menu_size,self.frame[i] * self.menu_size, #left, bottom
+            self.menu_sprite.clip_draw(i * self.menu_size,self.menu_frame * self.menu_size, #left, bottom
                                        self.menu_size,self.menu_size, #width,height - png안에서 너비
-                                       self.world_width - self.menu_gap-8,(7 - i) * (self.menu_size + 2.5) - 6,#x,y
+                                       self.world_width - self.menu_gap-8,(6-i) * (self.menu_size + 4) + 56,#x,y
                                        self.menu_gap,self.menu_gap)#width,height - 화면안에서 너비
     def update(self):
-        pass
+        self.menu_frame =(self.menu_frame + 1) % 2
+        
+        self.get_table_time += 1
+
+        
 
 class Worker:
     def __init__(self):
@@ -36,17 +42,21 @@ class Worker:
         self.dir = 0
         self.frame_x = 0
         self.frame_y = 0
-        self.image = load_image('character.png')
+        self.frame_size = 64
         self.speed = 0.2
         self.plate = [0, 0, 0, 0]
-        self.kimbap = load_image('kimbab.png')
-        self.fry = load_image('fry.png')
-        self.soondae = load_image('soondae.png')
-        self.ttukbokki = load_image('ttukbokki.png')
-        self.ramen = load_image('ramen.png')
-        self.water = load_image('water.png')
+        self.image = load_image('resource/character.png')
+        self.cooking_sprite = load_image('resource/food_sprite.png')
+        self.kimbap = load_image('resource/kimbap.png')
+        self.fry = load_image('resource/fry.png')
+        self.soondae = load_image('resource/soondae.png')
+        self.ttukbokki = load_image('resource/ttukbokki.png')
+        self.ramen = load_image('resource/ramen.png')
+        self.water = load_image('resource/water.png')
         
-        
+        self.cook_step = [0, 0, 0, 0, 0]
+        self.cook_type = 0 
+
     def update(self):
         if self.dir == 1: # 왼쪽으로
             if self.frame_x > -1:
@@ -77,40 +87,94 @@ class Worker:
                 self.y += 1
                 self.dir = 0
 
+        if self.cook_type == 3:
+            self.cook_step[0] += 1
+            self.cook_type = 0
+        elif self.cook_type == 4:
+            self.cook_step[0] += 1
+            self.cook_type = 0
+        elif self.cook_type == 5:
+            self.cook_step[0] += 1
+            self.cook_type = 0
+        elif self.cook_type == 6:
+            self.cook_step[0] += 1
+            self.cook_type = 0
+        elif self.cook_type == 7:
+            self.cook_step[0] += 1
+            self.cook_type = 0
+        elif self.cook_type == 8:
+            self.cook_step[0] += 1
+            self.cook_type = 0
     def draw(self):
         #플레이어 그리기
         self.image.draw((self.x + self.frame_x+2) * 75 ,(self.y + self.frame_y) * 67 + 75)
         #들고있는 메뉴그리기
-        for i in range(0,4):
+        gap = 50
+        for i in range(4):
             if i == 0 or i == 2:
                 if i == 0:
-                    xgap = -30
+                    xgap = -gap
                 else:
-                    xgap = 30
+                    xgap = gap
                 ygap = 0
             elif i == 1 or i == 3:
                 if i == 3:
-                    ygap = -30
+                    ygap = -gap
                 else:
-                    ygap = 30                
+                    ygap = gap                
                 xgap = 0
 
-            if self.plate[i] == 1: #떡볶이
-                self.ttukbokki.draw(self.x + xgap, self.y + ygap)
-            elif self.plate[i] == 2: #순대
-                self.soondae.draw(self.x + xgap, self.y + ygap)
-            elif self.plate[i] == 3:#김밥
-                self.kimbap.draw(self.x + xgap, self.y + ygap)
-            elif self.plate[i] == 4:#라면
-                self.ramen.draw(self.x + xgap, self.y + ygap)
-            elif self.plate[i] == 5:#후라이
-                self.fry.draw(self.x + xgap, self.y + ygap)
-            elif self.plate[i] == 6:#물
-                self.ttukbokki.draw(self.x + xgap, self.y + ygap)            
+            if self.plate[i] == 3: #라면
+                self.ramen.draw((self.x + self.frame_x+2) * 75 + xgap, (self.y + self.frame_y) * 67 + 75 + ygap)
+            elif self.plate[i] == 4: #순대
+                self.soondae.draw((self.x + self.frame_x+2) * 75 + xgap, (self.y + self.frame_y) * 67 + 75 + ygap)
+            elif self.plate[i] == 5:#김밥
+                self.kimbap.draw((self.x + self.frame_x+2) * 75 + xgap, (self.y + self.frame_y) * 67 + 75 + ygap)
+            elif self.plate[i] == 6:#떡볶이
+                self.ttukbokki.draw((self.x + self.frame_x+2) * 75 + xgap, (self.y + self.frame_y) * 67 + 75 + ygap)
+            elif self.plate[i] == 7:#튀김
+                self.fry.draw((self.x + self.frame_x+2) * 75 + xgap, (self.y + self.frame_y) * 67 + 75 + ygap)
+            elif self.plate[i] == 8:#물
+                self.water.draw((self.x + self.frame_x+2) * 75 + xgap, (self.y + self.frame_y) * 67 + 75 + ygap)
+        #만들어지는 메뉴 스프라이트 그리기
+        for i in range(5):
+            if self.cook_step[i] > 0:
+                self.cooking_sprite.clip_draw(self.frame_size * (self.cook_step[i]- 1),i * self.frame_size
+                                              ,self.frame_size,self.frame_size,
+                                               665,(6-i) * (self.frame_size + 4) + 56)
+
 
 class Table:
     def __init__(self):
-        self.table = load_image('table_sprite.png')
-        self.guset1 = load_image('guest01_sprite.png')
-        self.guset2 = load_image('guest02_sprite.png')
-    
+        self.order = []
+        self.step = 0
+        self.waiting_time = 0
+        self.is_active = False
+        self.status = 2
+        self.guest_amount = random.randint(1,3)    
+        self.guest1 = Guest()
+        self.guest2 = Guest()
+        #self.table_image = load_image('table_sprite.png')
+
+    def update(self):
+        if self.is_active:
+            self.waiting_time += 1
+            if self.waiting_time == 150:
+                self.status -= 1
+                self.waiting_time = 0
+    def draw(self):
+        pass
+class Guest:
+    def __init__(self):
+        self.type = random.randint(1,3)
+        #self.guset1_image = load_image('guest01_sprite.png')   
+        #self.guset2_image = load_image('guest02_sprite.png')        
+        if self.type == 1:
+            pass
+            #self.image = self.guset1_image
+        elif self.type == 2:
+            #elf.image = self.guset2_image
+            pass
+    def update(self):
+        pass
+
