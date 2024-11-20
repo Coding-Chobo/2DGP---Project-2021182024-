72a5e3f0c64969842ef76b5e3e1b0650463bca2d
Coding-Chobo <dbdaldrb1@gmail.com>
2024-11-21 04:42:08 +0900

    폰트로 맵 위치 따기

--- play_mode.py
+++ play_mode.py
@@ -24,37 +24,44 @@ def handle_events():
                 worker.rotate = True
             if worker.dir == 0: # 멈춰있을때
                 if event.key == SDLK_LEFT:
-                    cell = map.data[worker.y][worker.x - 1]
+                    cell = map.data[7 - worker.y][worker.x - 1]
                     if isinstance(cell, tuple):
-                        order = 0
                         table_number = cell[1]
-                        if map.tables[table_number].is_active:
-                            for i in range(4):
-                                if map.tables[table_number].step == 0 and worker.plate[i] == 8:
-                                    map.tables[table_number].step = 1
-                                    worker.plate[i] = 0
-                                    for n in range(map.tables[table_number].guest_amount):
-                                        order += map.tables[table_number].guest[n].type
-                                break
-                            for i in range(order): # 수정필요 딕셔너리 안에 키가 같으면 거기에 추가하는 형식으로 하고싶음 order의 딕셔너리를 기본값을 세팅해줘야됌
-                                map.tables[table_number].order = {random.randint(0,4)+ 3:1}
+                        t = map.tables[table_number]
+                        if t.is_active:
+                            if t.check_order(worker.plate[0]):                          
+                                worker.plate[0] = 0
                     else: 
-                        if cell == 0:
+                        if map.is_walkable(cell):
                             worker.dir = 1
-                        #달봉이(배달부)에게 전달하는 조건 추가
+                        #달봉이(배달부)에게 전달
+                        if map.data[7 - worker.y][worker.x] == 10:
+                            if type(map.alba) == Chunsik:
+                                print('춘식이 인식')
+                        
 
                 elif event.key == SDLK_RIGHT:
-                    if map.data[worker.y][worker.x + 1] == 0:
-                        worker.dir = 2
-                    elif map.data[worker.y][worker.x + 1] == 9: #쓰레기통
-                        if worker.plate[2] != 0:
-                            worker.plate[2] = 0
+                    cell = map.data[7 - worker.y][worker.x + 1]
+                    if isinstance(cell, tuple):
+                        table_number = cell[1]
+                        t = map.tables[table_number]
+                        if t.is_active:
+                            if t.check_order(worker.plate[2]):                               
+                                worker.plate[2] = 0
+                    else: 
+                        if map.is_walkable(cell) :
+                            worker.dir = 2  
+                        elif map.data[worker.y][worker.x + 1] == 9: #쓰레기통
+                            if worker.plate[2] != 0:
+                                worker.plate[2] = 0
                             
                 elif event.key == SDLK_UP:
-                    if map.data[worker.y + 1][worker.x] == 0:
+                    cell  = map.data[(worker.y + 1)][worker.x]
+                    if map.is_walkable(cell):
                         worker.dir = 4
                 elif event.key == SDLK_DOWN:
-                    if map.data[worker.y - 1][worker.x] == 0:
+                    cell  = map.data[(worker.y - 1)][worker.x]
+                    if map.is_walkable(cell):
                         worker.dir = 3
                 #음식 제조 및 상호작용키
                 elif event.key == SDLK_SPACE:
